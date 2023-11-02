d3f9dbc75f333b5d05efbfbd22eeeaa1060df5a7
Akshay Lavate <79072897+Akshay1299@users.noreply.github.com>
2023-11-02 20:49:31 +0530

    Time: 198 ms (92.62%), Space: 174.3 MB (83.13%) - LeetHub

--- 2339-k-divisible-elements-subarrays/2339-k-divisible-elements-subarrays.cpp
+++ 2339-k-divisible-elements-subarrays/2339-k-divisible-elements-subarrays.cpp
@@ -1,27 +1,61 @@
+// class Solution {
+// public:
+//     int countDistinct(vector<int>& nums, int k, int p) 
+//     {
+//         unordered_set<string> s; 
+//         int n=nums.size();
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             int count=0;
+//             string str="";
+            
+//             for(int j=i;j<n;j++)
+//             {
+//                 if(nums[j]%p==0)
+//                     count++;
+                
+//                 if(count>k)
+//                     break;
+                
+//                 str+=to_string(nums[j])+"->";
+//                 s.insert(str);
+//             }
+//         }
+//         return s.size();
+//     }
+// };
+
 class Solution {
 public:
-    int countDistinct(vector<int>& nums, int k, int p) 
-    {
-        unordered_set<string> s; 
-        int n=nums.size();
-        
-        for(int i=0;i<n;i++)
-        {
-            int count=0;
-            string str="";
-            
-            for(int j=i;j<n;j++)
-            {
-                if(nums[j]%p==0)
-                    count++;
-                
-                if(count>k)
+    class trie {
+        public:
+            unordered_map < int , trie* > next;
+            int c;
+            trie() {
+            }
+    };
+    int countDistinct(vector<int>& nums, int k, int p) {
+        trie *root = new trie();
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+
+            trie *temp = root;
+
+            int count = 0;
+            for(int j = i;j<n;j++){
+                if(nums[j] % p == 0)
+                    count++; 
+                if(count > k)
                     break;
-                
-                str+=to_string(nums[j])+"->";
-                s.insert(str);
+                if(!temp->next[nums[j]]){
+                    temp->next[nums[j]] = new trie();
+                    ans++;
+                }
+                temp = temp->next[nums[j]];
             }
         }
-        return s.size();
+        return ans;
     }
 };
