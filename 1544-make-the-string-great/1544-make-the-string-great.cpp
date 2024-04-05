ac5d8101018ccd373b9440dc540db9a0e5ca8b02
Akshay Lavate <79072897+Akshay1299@users.noreply.github.com>
2024-04-05 22:30:07 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (89.71%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+    string makeGood(string s) 
+    {
+        string ss;
+        
+        for (char c : s)
+            if (!ss.empty() && (ss.back() ^ c) == 32)
+                ss.pop_back();
+            else
+                ss.push_back(c);
+        
+        return ss;
+    }
+};
