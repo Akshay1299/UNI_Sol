a4422ab7ae45131eb9b812a88a4d684ef0fa4a5a
Akshay Lavate <79072897+Akshay1299@users.noreply.github.com>
2024-07-06 21:12:07 +0530

    Time: 2 ms (54.14%), Space: 7.2 MB (30.58%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int q = time/(n-1);
+        int rem = time%(n-1);
+
+        if(q%2 == 0){
+            return 1+rem;
+        }
+        return n-rem;
+    }
+};
