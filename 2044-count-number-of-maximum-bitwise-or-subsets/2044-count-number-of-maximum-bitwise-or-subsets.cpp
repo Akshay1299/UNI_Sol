f66ffa6dafba3d1fac0bcdcd62eb294790d9df42
Akshay Lavate <79072897+Akshay1299@users.noreply.github.com>
2024-10-18 19:18:10 +0530

    Time: 3 ms (99.75%), Space: 10.2 MB (48.34%) - LeetHub

---
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+private:
+    int BackTrack(vector<int>& nums, int idx, int curOrSum, int maxOrSum)
+    {
+        if (curOrSum == maxOrSum)
+        {
+            return pow(2, (nums.size() - idx));
+        }
+        else if (idx >= nums.size())
+        {
+            return 0;
+        }
+
+
+        return this->BackTrack(nums, idx + 1, curOrSum, maxOrSum) + this->BackTrack(nums, idx + 1, curOrSum | nums[idx], maxOrSum);
+    }
+
+public:
+    int countMaxOrSubsets(vector<int>& nums)
+    {
+        int maxOrNum = 0;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            maxOrNum |= nums[i];
+        }
+
+        sort(nums.begin(), nums.end(), greater<int>());
+
+        return this->BackTrack(nums, 0, 0, maxOrNum);
+    }
+};
